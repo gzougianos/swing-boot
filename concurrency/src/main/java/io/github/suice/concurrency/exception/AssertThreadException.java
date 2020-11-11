d37b272f4b8d2fd4e89862326dc4bf499a6222d1
gzougianos <g.zougianos@gmail.com>
2020-11-11 13:49:04 +0200

    concurrency module

---
+++ concurrency/src/main/java/io/github/suice/concurrency/exception/AssertThreadException.java
@@ -0,0 +1,11 @@
+package io.github.suice.concurrency.exception;
+
+public class AssertThreadException extends RuntimeException {
+
+	private static final long serialVersionUID = 5208734274016003414L;
+
+	public AssertThreadException(String message) {
+		super(message);
+	}
+
+}
