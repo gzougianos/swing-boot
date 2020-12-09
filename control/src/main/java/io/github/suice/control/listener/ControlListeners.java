0727dfa83066bde4096aa2390aa691eaa8b1ddc5
gzougianos <g.zougianos@gmail.com>
2020-12-09 03:38:04 +0200

    control listeners get all method

--- control/src/main/java/io/github/suice/control/listener/ControlListeners.java
+++ control/src/main/java/io/github/suice/control/listener/ControlListeners.java
@@ -1,7 +1,12 @@
 package io.github.suice.control.listener;
 
+import static java.util.Collections.unmodifiableList;
+
 import java.awt.Component;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.EventListener;
+import java.util.List;
 
 public final class ControlListeners {
 	private ControlListeners() {
@@ -15,6 +20,17 @@ public final class ControlListeners {
 			}
 		}
 		return null;
+	}
+
+	public static <T extends EventListener> Collection<T> getAll(Class<T> listenerType, Component component) {
+		T[] listeners = component.getListeners(listenerType);
+		List<T> result = new ArrayList<>();
+		for (T listener : listeners) {
+			if (listener instanceof ControlListener) {
+				result.add(listener);
+			}
+		}
+		return unmodifiableList(result);
 	}
 
 }
--- control/src/test/java/io/github/suice/control/listener/ControlListenersTests.java
+++ control/src/test/java/io/github/suice/control/listener/ControlListenersTests.java
@@ -1,10 +1,12 @@
 package io.github.suice.control.listener;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
 
 import javax.swing.JButton;
 
@@ -22,12 +24,24 @@ class ControlListenersTests {
 		Listener listener = new Listener();
 		button.addActionListener(listener);
 		assertEquals(listener, ControlListeners.get(ActionListener.class, button));
+
+		Collection<ActionListener> all = ControlListeners.getAll(ActionListener.class, button);
+		assertEquals(1, all.size());
+		assertTrue(all.contains(listener));
+
+		Listener listener2 = new Listener();
+		button.addActionListener(listener2);
+		all = ControlListeners.getAll(ActionListener.class, button);
+		assertEquals(2, all.size());
+		assertTrue(all.contains(listener2));
+		assertTrue(all.contains(listener));
 	}
 
 	@UiTest
 	void listenerDoesNotExist() {
 		JButton button = new JButton();
 		assertNull(ControlListeners.get(ActionListener.class, button));
+		assertTrue(ControlListeners.getAll(ActionListener.class, button).isEmpty());
 	}
 
 	private static class Listener implements ActionListener, ControlListener {
